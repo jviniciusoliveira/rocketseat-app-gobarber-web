099b9c532a7e902823287b64f21c94cad44d1378
Jose Vinicius <jviniciusoliveira@gmail.com>
2020-04-09 16:01:52 -0300

    feat: add default layouts and global styles

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import { Router } from 'react-router-dom';
 import Routes from './routes';
 import history from './services/history';
 
+import GlobalStyle from './styles/global';
+
 import './config/reactotron';
 
 function App() {
   return (
     <Router history={history}>
       <Routes />
+      <GlobalStyle />
     </Router>
   );
 }
