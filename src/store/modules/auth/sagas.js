411d1aed2a4a089da1d65b48c51514773e0b4ce9
Jose Vinicius <jviniciusoliveira@gmail.com>
2020-04-11 15:18:12 -0300

    feat: add action for signup

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -25,4 +25,22 @@ export function* signIn({ payload }) {
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', { name, email, password, provider: true });
+
+    toast.success('Usuário cadastrado com sucesso.');
+    history.push('/');
+  } catch (error) {
+    toast.error('Falha ao cadastrar, Tente novamente.');
+
+    yield put(signFailure());
+  }
+}
+
+export default all([
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
